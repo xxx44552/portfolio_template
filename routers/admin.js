cdc23daa07cd0308e8a308ffa5f8ad0cebb99ebc
Artur Kudaev <artur.kudaev@ria.com>
2020-07-06 22:14:13 +0300

    fix router src

--- routers/admin.js
+++ routers/admin.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const auth = require("./../middleware/auth");
 
 router.get('/admin', auth, function (req, res) {
-  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 module.exports = router;
